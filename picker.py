4e27dfd3c16a2ee5c530d006b98a0dfa7f9fbf49
Encant YT <68126135+EncantYT@users.noreply.github.com>
2020-10-31 23:45:14 +0100

    Update picker.py

--- picker.py
+++ picker.py
@@ -3,13 +3,13 @@ import time
 
 
 #Intro
-
 print("Hello, welcome to the Linux distribution picker (The list is from Wikipedia) anyway wait some seconds and your distro will be picked!")
 time.sleep(3)
 print("Fun fact: this list has 249 lines of text!")
 time.sleep(2)
 print("Picking...")
 time.sleep(1)
+
 #Final
 distrochosen = line = random.choice(open('Linuxlist.txt').readlines())
 
